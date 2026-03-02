9926eb4eb69b85b2781c2b458e5adb99039eb728
seb-dalde <seb.dalde.scc@gmail.com>
2026-03-02 19:31:50 +0800

    Create BasicSwing.java

---
+++ 2ND SEMESTER/Assignments/BasicSwing.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class BasicSwing {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My First Swing Window");
+
+        frame.setSize(400, 300);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
